aa62ec436cdcce91f385d612dec3048e37302b56
Penghk <1769476788@qq.com>
2021-02-18 09:46:23 +0800

    Promise实例状态的改变与结果的保存（PromiseStat/PromiseResult属性）

--- src/js/Promise.js
+++ src/js/Promise.js
@@ -1,16 +1,21 @@
 class Promise {
+  PromiseState = "pending"
+  PromiseResult = null
   constructor(executor) {
+    // 改变Promise实例的状态，保存执行的结果
     const resolve = (data) => {
-
+      this.PromiseState = "fulfilled"
+      this.PromiseResult = data
     }
     const reject = (data) => {
-
+      this.PromiseState = "fulfilled"
+      this.PromiseResult = data
     }
-
+    // 创建Promise对象时，同步执行回调函数
     executor(resolve, reject)
   }
 
   then(onResolved, onRejected) {
-    
+
   }
 }
