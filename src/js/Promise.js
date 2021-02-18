b6eeb9548003851fb86a191a3c157dd2e00d2f3b
Penghk <1769476788@qq.com>
2021-02-18 09:49:20 +0800

    抛出异常的处理

--- src/js/Promise.js
+++ src/js/Promise.js
@@ -4,15 +4,22 @@ class Promise {
   constructor(executor) {
     // 改变Promise实例的状态，保存执行的结果
     const resolve = (data) => {
+      // 修改状态
       this.PromiseState = "fulfilled"
+      // 保存数据
       this.PromiseResult = data
     }
     const reject = (data) => {
-      this.PromiseState = "fulfilled"
+      this.PromiseState = "rejected"
       this.PromiseResult = data
     }
     // 创建Promise对象时，同步执行回调函数
-    executor(resolve, reject)
+    try {
+      executor(resolve, reject)
+    } catch (error) {
+      // 如果出现异常直接调用reject方法进行处理
+      reject(error)
+    }
   }
 
   then(onResolved, onRejected) {
